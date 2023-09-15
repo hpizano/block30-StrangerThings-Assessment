a66350a96b8827b7b1bdd84634bf7828124eda66
Hector Pizano <pizano_hector@outlook.com>
2023-09-15 11:03:12 -0500

    set button disabled when no input is made

---
+++ src/ContactUs.jsx
@@ -0,0 +1,10 @@
+const ContactUs = () => {
+    return (
+    <>
+    <h1> Hello Stranger</h1>
+    <p> Please fill out the form bellow to contact us!</p>
+    </>
+    )
+}
+
+export default ContactUs
